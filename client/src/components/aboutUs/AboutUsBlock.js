90c8db01c013dabb16f5b78660e8aa5e1e16e6fa
Sherali5543 <shaheerazamkhan@gmail.com>
2024-06-11 22:36:41 -0600

    About us text spacing resolved

--- client/src/components/aboutUs/AboutUsBlock.js
+++ client/src/components/aboutUs/AboutUsBlock.js
@@ -20,7 +20,7 @@ var AboutUsBlock = React.forwardRef((props, ref) => {
         <div style={aboutUsStyle()} className="block" id={id} ref={ref}>
             <div className="about-us-text" id="about-us-text" style={{paddingBottom: '10px'}}>
                 <h1 className='main-heading faded text-main'>About Us</h1>
-                <h2 className='sub-heading faded text-main colored-text'>The Computer Engineering Club</h2>
+                <h2 className='sub-heading-about-us faded text-main colored-text'>The Computer Engineering Club</h2>
                 <p>
                     For over 35 years, the University of Alberta's Computer Engineering Club has brought Computer Engineering students together, organized specialized events and initiatives, and advocated to larger bodies — such as the Engineering Students’ Society, the Faculty of Engineering, and the Students’ Union. In recent years however, we have expanded our scope to include students in Computer Science, and Data Science fields who share similar interests in industry.
                     We have a longstanding history of running large-scale events focused around educational and hands-on learning for programming and design, such as <a href="https://hacked.compeclub.com" target="_blank" rel="noopener noreferrer">HackED</a> and <a href="https://hackedbeta.compeclub.com" target="_blank" rel="noopener noreferrer">HackED Beta</a>. 
