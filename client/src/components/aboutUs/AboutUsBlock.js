fc1e2e715f34c3370bed838755d0853cd3d61f31
Sherali5543 <shaheerazamkhan@gmail.com>
2024-06-11 23:43:09 -0600

    various format changes

--- client/src/components/aboutUs/AboutUsBlock.js
+++ client/src/components/aboutUs/AboutUsBlock.js
@@ -4,8 +4,6 @@ import navLinker from "../pageState/observer/navLinker";
 
 const aboutUsStyle = () => ({
     display: 'flex',
-    // 'padding-left': '5vw',
-    // 'border-bottom': '1px solid gray',
     'align-items': 'flex-start',
     'overflow': 'hidden'
 }
@@ -22,17 +20,28 @@ var AboutUsBlock = React.forwardRef((props, ref) => {
                 <h1 className='main-heading faded text-main'>About Us</h1>
                 <h2 className='sub-heading-about-us faded text-main colored-text'>The Computer Engineering Club</h2>
                 <p>
-                    For over 35 years, the University of Alberta's Computer Engineering Club has brought Computer Engineering students together, organized specialized events and initiatives, and advocated to larger bodies — such as the Engineering Students’ Society, the Faculty of Engineering, and the Students’ Union. In recent years however, we have expanded our scope to include students in Computer Science, and Data Science fields who share similar interests in industry.
-                    We have a longstanding history of running large-scale events focused around educational and hands-on learning for programming and design, such as <a href="https://hacked.compeclub.com" target="_blank" rel="noopener noreferrer">HackED</a> and <a href="https://hackedbeta.compeclub.com" target="_blank" rel="noopener noreferrer">HackED Beta</a>. 
-                    Now, we are excited to continue this fairly new endeavor, with the <strong className="colored-text">goal of connecting undergraduate students to employers and businesses</strong> in a way that is unprecedented on our campus: Aether
+                    For over 35 years, the University of Alberta's Computer Engineering Club has brought 
+                    Computer Engineering students together, organized specialized events and initiatives, 
+                    and advocated to larger bodies — such as the Engineering Students' Society, the Faculty 
+                    of Engineering, and the Students' Union. In recent years, however, we have expanded our 
+                    scope to include students in Computer Science, and Data Science fields who share similar interests in industry.
                 </p>
                 <p>
-                    While Aether is still somewhat new, our club has a history of running large-scale successful events. One of these events is HackED, <strong className="colored-text">Alberta’s largest student-run hackathons with over 700 participants</strong>, and a history stretching back over 10 years. 
-                    Additionally, at the inaugeral Aether (previously Career++) last year, we had an incredible participation rate of <strong className="colored-text">over 1000 students</strong> and received <strong className="colored-text">overwhelmingly positive feedback</strong> from the students and companies! 
-                    Furthermore, many students successfully received internship or new grad job offers directly from this event!
+                    We have a longstanding history of running large-scale events focused on educational and hands-on learning for programming and design, such as <a href="https://hacked.compeclub.com" target="_blank" rel="noopener noreferrer">HackED</a> and <a href="https://hackedbeta.compeclub.com" target="_blank" rel="noopener noreferrer">HackED Beta</a>, <strong className="colored-text">Alberta's largest student-run hackathons with over 700 participants</strong>.
+                    Now, we are excited to continue this fairly new endeavor, <strong className="colored-text">to connect 
+                    undergraduate students to employers and businesses</strong> in a way that is unprecedented on our campus: Aether
+                </p>
+                <p>
+                    While Aether is still somewhat new, last year we had an incredible participation 
+                    rate of over 1000 students and received overwhelmingly positive feedback from the 
+                    students and companies! Furthermore, many students successfully received internship 
+                    or new grad job offers directly from this event! 
                     Some of the companies and organizations that attended previously are:
-                    <ul>Servus Credit Union, DriveWyze, Inflexion Games, Technology Alberta, PCL, 
-                    Drugbank, Sparrow Connected, Fig Finance, DevFacto, Blue Marvel Ai/Spartan Controls, Lantern, Amii, IEEE, Microchip, and Paper Leaf</ul>
+                    <ul>
+                        Servus Credit Union, DriveWyze, Inflexion Games, Technology Alberta, PCL, 
+                        Drugbank, Sparrow Connected, Fig Finance, DevFacto, Blue Marvel Ai/Spartan Controls, 
+                        Lantern, Amii, IEEE, Microchip, and Paper Leaf
+                    </ul>
                     <br/><strong className="colored-text">Check out the images below to get a better sense of this event!</strong>
                 </p>
             </div>
