988e0377c93839af22ac8083a212d03dbf2a7b94
Sherali5543 <shaheerazamkhan@gmail.com>
2024-06-15 22:40:12 -0600

    Changing photos

--- client/src/components/aboutUs/PrevCareerFairBlock.js
+++ client/src/components/aboutUs/PrevCareerFairBlock.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import withAutoplay from 'react-awesome-slider/dist/autoplay';
-import image1 from "./careerphotos/Career1.jpg";
-import image2 from "./careerphotos/Career2.jpg";
-import image3 from "./careerphotos/Career3.jpg";
-import image4 from "./careerphotos/Career4.jpg";
-import image5 from "./careerphotos/Career5.jpg";
-import image6 from "./careerphotos/Career6.jpg";
+import image1 from "./careerphotos/AetherPhoto1.jpg";
+import image2 from "./careerphotos/AetherPhoto2.jpg";
+import image3 from "./careerphotos/AetherPhoto3.jpg";
+import image4 from "./careerphotos/AetherPhoto4.jpg";
+import image5 from "./careerphotos/AetherPhoto5.jpg";
+import image6 from "./careerphotos/AetherPhoto6.jpg";
+import image7 from "./careerphotos/AetherPhoto7.jpg";
 import AwesomeSlider from 'react-awesome-slider';
 import 'react-awesome-slider/dist/styles.css';
 
@@ -33,6 +34,7 @@ const PrevCareerFairBlock = () => {
       <div data-src={image4} />
       <div data-src={image5} />
       <div data-src={image6} />
+      <div data-src={image7} />
     </AutoplaySlider>
   </div>
   );
