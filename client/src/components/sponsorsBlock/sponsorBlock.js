fbc71dbb22ae4ece42fab062caef2ca5069fef6e
davidurschel <durschel@ualberta.ca>
2024-06-22 14:32:16 -0700

    Centered sponsor block

--- client/src/components/sponsorsBlock/sponsorBlock.js
+++ client/src/components/sponsorsBlock/sponsorBlock.js
@@ -28,7 +28,7 @@ const sponsorBlock = React.forwardRef((props, ref) => {
 
     return (
         <div style={blockStyle()} className="block" id={id} ref={ref}>
-            <div>
+            <div className="sponsor-card-block">
                 <h1 className='sponsorBlockHeading heading'>We couldn't have done this event without...</h1>
                 {Object.keys(groupedSponsors).map( (tier) => (
                     <SponsorSection key={tier} tier={tier} sponsors={groupedSponsors[tier]}/>
