601bdd033daae443a60ff99d5e91062c7818ab97
Sherali5543 <shaheerazamkhan@gmail.com>
2024-06-23 11:26:37 -0600

    Resize sponsorblock header for mobile view to center block

--- client/src/components/sponsorsBlock/sponsorBlock.js
+++ client/src/components/sponsorsBlock/sponsorBlock.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react'
+import React, { useEffect } from 'react'
 import sponsorData from './sponsorInfo.json'
 import blockStyle from '../Block';
 import navLinker from '../pageState/observer/navLinker';
@@ -13,7 +13,7 @@ const sponsorBlock = React.forwardRef((props, ref) => {
 
     function groupBySponsorTitle(sponsorArray) {
         return sponsorArray.reduce((acc, sponsor) => {
-            if(!acc[sponsor.tier])
+            if (!acc[sponsor.tier])
                 acc[sponsor.tier] = [];
             acc[sponsor.tier].push(sponsor);
             return acc;
@@ -28,30 +28,32 @@ const sponsorBlock = React.forwardRef((props, ref) => {
 
     return (
         <div style={blockStyle()} className="block" id={id} ref={ref}>
-            <div className="sponsor-card-block">
-                <h1 className='sponsorBlockHeading heading'>We couldn't have done this event without...</h1>
-                {Object.keys(groupedSponsors).map( (tier) => (
-                    <SponsorSection key={tier} tier={tier} sponsors={groupedSponsors[tier]}/>
-                ))}
-            </div>
+            {
+                visible && <div className="sponsor-card-block">
+                    <h1 className='sponsorBlockHeading heading'>We couldn't have done this event without...</h1>
+                    {Object.keys(groupedSponsors).map((tier) => (
+                        <SponsorSection key={tier} tier={tier} sponsors={groupedSponsors[tier]} />
+                    ))}
+                </div>
+            }
         </div>
     )
 });
 
-const SponsorSection = ({tier, sponsors}) => (
+const SponsorSection = ({ tier, sponsors }) => (
     <div className={`sponsor-section ${tier.toLowerCase()}`}>
-        {(tier=='Community')
-        ?(<h2>{tier} Partners</h2>)
-        :(<h2>{tier} Sponsors</h2>)}
+        {(tier == 'Community')
+            ? (<h2>{tier} Partners</h2>)
+            : (<h2>{tier} Sponsors</h2>)}
         <div className='sponsor-cards'>
             {sponsors.map((sponsor) => (
-                <SponsorCard key={sponsor.name} sponsor={sponsor}/>
+                <SponsorCard key={sponsor.name} sponsor={sponsor} />
             ))}
         </div>
     </div>
 );
 
-const SponsorCard = ({sponsor}) => (
+const SponsorCard = ({ sponsor }) => (
     <div className="sponsor-card">
         <a href={sponsor.website} className='sponsor-site' target='_blank'>
             <img src={sponsor.logo} alt={`${sponsor.name} company logo`}></img>
